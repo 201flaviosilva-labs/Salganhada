e6cfd6d9553ca25d8e530b829c350f5056ba64ec
Flavio Silva <flavioandre.2001.9.7@gmail.com>
2020-09-18 23:49:48 +0100

    Mudar o objeto de conquista de Moeda para um camarão

--- Script/camarao.js
+++ Script/camarao.js
@@ -1,11 +1,11 @@
-class Moeda {
+class Camarao {
 	constructor() {
-		const tamanho = Math.floor(Math.random() * 15) + 15;
+		const tamanho = Math.floor(Math.random() * 15) + 20;
 		this.Width = tamanho;
 		this.Height = tamanho;
 		do this.Y = Math.floor(Math.random() * canvasHeight - tamanho); while (this.Y < 0);
 		do this.X = Math.floor(Math.random() * canvasWidth - tamanho); while (this.X < 0);
 		this.sprite = new Image();
-		this.sprite.src = "../Assets/Coin.png";
+		this.sprite.src = "../Assets/camarao.png";
 	}
 }
